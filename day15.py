cdf32dd0db018b53c2046b2826e7b287007d2352
Brendan <smallpigeon1@gmail.com>
2021-12-27 11:54:18 -0800

    Add part 2 (slow)

--- day15.py
+++ day15.py
@@ -10,6 +10,11 @@ class Point:
     if (vertexToLink not in self.linked):
       self.linked.add(vertexToLink)
 
+  def incrementWeight(self, amount: int):
+    self.weight += amount
+    if (self.weight > 9):
+      self.weight -= 9
+
 class PointWrapper:
   """Point wrapper for use in the priority queue."""
   def __init__(self, point: Point, previous: Point, dist: int):
@@ -27,42 +32,60 @@ class Graph:
 
   def dijkstra(self):
     startIndex = 0
-    # Store final distance from start
-    # PointWrapper stores distance in queue, which is compared to final distance
-    # in while loop
-    # If new distance is lower than final distance, replace with new distance
-    pathValues = [0]
-    pathValues.extend([float('inf')] * (len(self.graph) - 1))
+    result = 0
 
-    resolved: "list[Point]" = []
-    # currentVertex = startIndex
+    resolved: "set[Point]" = set()
 
     # Track adjacency (points that need to be "served")
     # Priority queue based on distance from start
-    queue: "list[PointWrapper]" = [PointWrapper(self.graph[startIndex], self.graph[startIndex], 0)]
+    queue: "set[PointWrapper]" = set([PointWrapper(self.graph[startIndex], self.graph[startIndex], 0)])
     
     while (len(resolved) < len(self.graph)):
       # Get point with minimum distance value (priority)
       point = min(queue, key=lambda item: item.dist)
 
-      if (point.point not in resolved):
-        resolved.append(point.point)
+      for newPoint in [pt for pt in point.point.linked if pt not in resolved]:
+        newDist = point.dist + newPoint.weight
+        
+        # Append to queue
+        queue.add(PointWrapper(newPoint, point.point, newDist))
+
+        # Add new point to resolved now, so it doesn't get added again to queue later
+        resolved.add(newPoint)
+
+        # # Compare with value in pathValues
+        index = self.graph.index(newPoint)
+        if (index == len(self.graph) - 1):
+          result = newDist
+          break
 
       queue.remove(point)
 
-      for newPoint in point.point.linked:
-        if (newPoint not in resolved):
-          existing = [item for item in queue if item.point == newPoint and item.previous == point.point]
-          if (len(existing) == 0):
-            queue.append(PointWrapper(newPoint, point.point, point.dist + newPoint.weight))
-          
-            # Compare with value in pathValues
-            index = self.graph.index(newPoint)
-            if (pathValues[index] > point.dist + newPoint.weight):
-              pathValues[index] = point.dist + newPoint.weight
+    return result
 
-    return pathValues[-1]
+  @staticmethod
+  def linkPoints(pointList: "list[list[Point]]"):
+    """Link the points inside the pointList, and return a list of all points."""
+    points: "list[Point]" = []
+    for i in range(len(pointList)): # row
+      for j in range(len(pointList[i])): # column
+        # pointsToLink = []
+        
+        if (i > 0):
+          pointList[i][j].addLinked(pointList[i - 1][j])
 
+        if (i < len(pointList) - 1):
+          pointList[i][j].addLinked(pointList[i + 1][j])
+
+        if (j > 0):
+          pointList[i][j].addLinked(pointList[i][j - 1])
+
+        if (j < len(pointList[i]) - 1):
+          pointList[i][j].addLinked(pointList[i][j + 1])
+
+        points.append(pointList[i][j])
+
+    return points
 
 if (__name__ == "__main__"):
   startTime = time.perf_counter()
@@ -70,28 +93,44 @@ if (__name__ == "__main__"):
   with open('inputFiles/day15.txt', 'r') as inputFile:
     inputFromFile = inputFile.read()
 
-  points: "list[Point]" = []
   pointList = [[Point(int(i)) for i in row] for row in inputFromFile.split('\n')]
-  for i in range(len(pointList)): # row
-    for j in range(len(pointList[i])): # column
-      # pointsToLink = []
-      
-      if (i > 0):
-        pointList[i][j].addLinked(pointList[i - 1][j])
-
-      if (i < len(pointList) - 1):
-        pointList[i][j].addLinked(pointList[i + 1][j])
-
-      if (j > 0):
-        pointList[i][j].addLinked(pointList[i][j - 1])
-
-      if (j < len(pointList[i]) - 1):
-        pointList[i][j].addLinked(pointList[i][j + 1])
-
-      points.append(pointList[i][j])
+  points = Graph.linkPoints(pointList)
 
   graph = Graph(points)
   print(f"Lowest total risk for part 1: {graph.dijkstra()}")
 
+  # Reset point list
+  pointList = [[Point(int(i)) for i in row] for row in inputFromFile.split('\n')]
 
+  # Extend pointList for part 2
+  # Extend horizontally
+  for row in range(len(pointList)):
+    rowCopy = pointList[row].copy()
+
+    for i in range(1, 5):
+      copied = [Point(item.weight) for item in rowCopy]
+      for item in copied:
+        item.incrementWeight(i)
+
+      pointList[row].extend(copied)
+
+  # Extend vertically
+  originalLength = len(pointList)
+  i = 1
+  while (len(pointList) < originalLength * 5):
+    for row in range(originalLength):
+      copied = [Point(item.weight) for item in pointList[row]]
+      for item in copied:
+        item.incrementWeight(i)
+
+      pointList.append(copied)
+
+    i += 1
+
+  # Re-link points
+  points = Graph.linkPoints(pointList)
+
+  graph = Graph(points)
+
+  print(f"Lowest total risk for part 2: {graph.dijkstra()}")
   print("--- %s seconds ---" % (time.perf_counter() - startTime))
