e38af96b5d2411c3cc05e8f04b0439ddcb5af9dc
Brendan <smallpigeon1@gmail.com>
2022-01-01 17:29:03 -0800

    Add part 2

--- day16.py
+++ day16.py
@@ -28,6 +28,81 @@ def convertToBin(hexValue: str):
 
   return binValue
 
+def evaluateValues(binary: str):
+  """Decode the given packet and return the evaluation of the values."""
+  if (len(binary) <= 7):
+    # Invalid packet
+    return (0, len(binary))
+
+  # Check packet type ID
+  ptId = int(binary[3:6], 2)
+  endIndex = 0
+  value = 0
+
+  # Values of each individual subpacket
+  tempValues = []
+
+  if (ptId == 4):
+    tempStr = '';
+    tempBin = '';
+    parseStart = 6
+    # Parse groups until end group reached
+    while (tempStr != '0'):
+      tempStr = binary[parseStart]
+      tempBin += binary[parseStart + 1:parseStart + 5]
+      parseStart += 5
+
+    value = int(tempBin, 2)
+    endIndex = parseStart
+    return (value, endIndex)
+
+  # Switch between ptId values
+  ltId = binary[6]
+  if (ltId == '0'):
+    # Check next 15 bits for packet length
+    packetLength = int(binary[7:22], 2)
+    nextStr = binary[22:22 + packetLength]
+    endIndex = 22
+
+    while (len(nextStr) > 0):
+      result = evaluateValues(nextStr)
+      tempValues.append(result[0])
+      endIndex += len(nextStr) - len(nextStr[result[1]:])
+      nextStr = nextStr[result[1]:]
+
+  elif (ltId == '1'):
+    # Check next 11 bits for # of subpackets
+    numPackets = int(binary[7:18], 2)
+    tempStartIndex = 0
+    endIndex = 18
+
+    for _ in range(numPackets):
+      result = evaluateValues(binary[18 + tempStartIndex:])
+      tempValues.append(result[0])
+      tempStartIndex += result[1]
+
+    endIndex += tempStartIndex
+
+  # Do something with tempValues based on the ptId
+  if (ptId == 0):
+    value = sum(tempValues)
+  elif (ptId == 1):
+    value = 1
+    for i in tempValues:
+      value *= i
+  elif (ptId == 2):
+    value = min(tempValues)
+  elif (ptId == 3):
+    value = max(tempValues)
+  elif (ptId == 5):
+    value = 1 if tempValues[0] > tempValues[1] else 0
+  elif (ptId == 6):
+    value = 1 if tempValues[0] < tempValues[1] else 0
+  elif (ptId == 7):
+    value = 1 if tempValues[0] == tempValues[1] else 0
+
+  return (value, endIndex)
+
 def sumVersionNumbers(binary: str):
   """Decode the given packet and return the version number sum."""
   if (len(binary) <= 7):
@@ -69,7 +144,7 @@ def sumVersionNumbers(binary: str):
       tempStartIndex = 0
       endIndex = 18
 
-      for i in range(numPackets):
+      for _ in range(numPackets):
         result = sumVersionNumbers(binary[18 + tempStartIndex:])
         version += result[0]
         tempStartIndex += result[1]
@@ -87,5 +162,6 @@ if (__name__ == "__main__"):
   binary = convertToBin(inputFromFile)
 
   print(f'Sum of version numbers: {sumVersionNumbers(binary)[0]}')
+  print(f'Evaluation of values: {evaluateValues(binary)[0]}')
 
   print("--- %s seconds ---" % (time.perf_counter() - startTime))
